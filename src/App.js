4a088cd456a838005965e06af0707dfd9c4d9eb2
preveenraj <97preveenraj@gmail.com>
2021-05-30 22:57:58 +0530

    add tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <h1>Chilltech Cooling Systems</h1>
+      <header className="flex h-screen justify-center items-center bg-blue-600">
+      <h1 className="text-white font-bold text-5xl">Chilltech Cooling Systems</h1>
       </header>
     </div>
   );
